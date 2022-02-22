9ae1d6b7723019d28f9160605bc61b300d6537a1
WalterIran <amadorwalter3418@gmail.com>
2022-02-22 13:18:41 -0600

    Folder structure initialized

---
+++ middlewares/error.handler.js
@@ -0,0 +1,27 @@
+const boom = require('@hapi/boom');
+
+//Middleware to log errors in console
+function logErrors (err, req, res, next) {
+  console.log(err);
+  next(err);
+}
+
+//Middleware to send errors to client
+function errorHandler(err, req, res, next) {
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+}
+
+//Middleware to handle boom errors
+function boomErrorHandler(err, req, res, next) {
+  if (err.isBoom) {
+
+    const { output } = err;
+    res.status(output.statusCode).json(output.payload);
+  }
+  next(err);
+}
+
+module.exports = { logErrors, errorHandler, boomErrorHandler, ormErrorHandler }
