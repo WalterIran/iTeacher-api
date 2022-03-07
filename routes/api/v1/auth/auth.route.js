022345271de536d6d0d7481622fdc619605b6b7f
Oscar Andrade <oandrade57@gmail.com>
2022-03-06 21:08:23 -0600

    Teacher Registration API

---
+++ routes/api/v1/auth/auth.route.js
@@ -0,0 +1,58 @@
+const router = require('express').Router();
+const jwt = require('jsonwebtoken');
+const boom = require('@hapi/boom');
+
+//Validation
+const validatorHandler = require('../../../../middlewares/validator.handler');
+const { teacherSignUpSchema, studentSignUpSchema, loginSchema } = require('../../../../schemas/auth.schema');
+
+//Models
+const User = require('../../../../dao/users/user.model');
+const userModel = new User();
+
+//Utils
+const { hashPassword, comparePassword } = require('../../../../utils/encryption.utils');
+
+router.post('/teacher-signup',
+    validatorHandler(teacherSignUpSchema, 'body'),
+    async (req, res, next) => {
+        try {
+            const data = req.body;
+            const insertData = {
+                username: data.name + ' ' + data.surname,
+                teacherType: data?.teacherType,
+                userType: 'teacher',
+                about: {
+                    degreeName: data?.degreeName,
+                    description: data?.aboutDescription,
+                    institutions: data?.institutions,
+                    platforms: data?.platforms
+                },
+                email: data.email,
+                password: await hashPassword(data.password),
+            }
+
+            const result = await userModel.new(insertData);
+
+            const payload = {
+                userId: result.insertedId,
+                userType: 'teacher',
+                email: data.email
+            }
+
+            const accessToken = jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: '10d'});
+            
+            res.status(200).json({
+                status: 'success',
+                result,
+                accessToken
+            });
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
+
+
+module.exports = router;
